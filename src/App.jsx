192f7c3f6166c9bca75f4c309d68b8412352ea4a
Ahren Agatep <36459220+ahrenagatep@users.noreply.github.com>
2024-09-23 01:18:59 -0400

    added sketchy dark/light mode lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import './App.css'
 import Navbar from './components/NavBar/Navbar'
+import { useState } from 'react';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   return (
-    <>
+    <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
     <div>
       <Navbar />
     </div>
-    <h1 className='title'>MyAnimeList If It Wasn't Mid</h1>
-      <h4 className="about-us">
-        developed by ahren agatep and khushi khan :3
+    <button onClick={toggleTheme}>
+      Switch to {isDarkMode ? 'Light' : 'Dark'} Mode
+    </button>
+    <h1 className='title dark-mode'>MyAnimeList if it Wasn't Mid</h1>
+    <h4 className="about-us">
+      developed by ahren agatep and khushi khan :3
       </h4>
-    </>
+    </div>
   )
 }
 
